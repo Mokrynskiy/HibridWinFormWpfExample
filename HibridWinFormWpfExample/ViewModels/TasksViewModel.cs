107787718565340168a5ecd3f1ef783004be3390
mokrinskiy <mokrynskiy@gmail.com>
2022-02-20 18:13:05 +0700

    Создание viewModel

---
+++ HibridWinFormWpfExample/ViewModels/TasksViewModel.cs
@@ -0,0 +1,34 @@
+﻿using DevExpress.Mvvm;
+using HibridWinFormWpfExample.Data;
+using HibridWinFormWpfExample.Data.Repositories;
+using HibridWinFormWpfExample.Mappers;
+using HibridWinFormWpfExample.Models;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Windows.Input;
+
+namespace HibridWinFormWpfExample.ViewModels
+{
+    public class TasksViewModel: ViewModelBase
+    {
+        private TaskRepository _repository;
+        public ObservableCollection<TaskModel> Tasks { get; set; }
+        public TaskModel SelectedTask { get; set; }
+        public TasksViewModel()
+        {
+            Tasks = new ObservableCollection<TaskModel>();
+            SelectedTask = null;
+            _repository = new TaskRepository(new TaskDbContext());
+            LoadData();
+        }
+        private void LoadData()
+        {
+            foreach (var task in _repository.GetAll())
+            {
+                Tasks.Add(task.ToModel());
+            }
+            SelectedTask = Tasks.FirstOrDefault();
+        }
+        
+    }
+}
