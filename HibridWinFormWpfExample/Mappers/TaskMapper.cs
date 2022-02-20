8ff4fa1dc5ccb5b2720e5e2d7334de64ae79a0ad
mokrinskiy <mokrynskiy@gmail.com>
2022-02-20 18:04:19 +0700

    Сождание модели данных и mapper

---
+++ HibridWinFormWpfExample/Mappers/TaskMapper.cs
@@ -0,0 +1,17 @@
+﻿using HibridWinFormWpfExample.Data.Entities;
+using HibridWinFormWpfExample.Models;
+
+namespace HibridWinFormWpfExample.Mappers
+{
+    public static class TaskMapper
+    {
+        public static TaskEntity ToEntity(this TaskModel model)
+        {
+            return new TaskEntity { Id = model.Id, CreateDate = model.CreateDate, DueDate = model.DueDate, CompletionDate = model.CompletionDate, TaskTitle = model.TaskTitle, TaskDiscription = model.TaskDiscription };
+        }
+        public static TaskModel ToModel(this TaskEntity entity)
+        {
+            return new TaskModel { Id = entity.Id, CreateDate = entity.CreateDate, DueDate = entity.DueDate, CompletionDate = entity.CompletionDate, TaskTitle = entity.TaskTitle, TaskDiscription = entity.TaskDiscription };
+        }
+    }
+}
---
+++ HibridWinFormWpfExample/Models/TaskModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace HibridWinFormWpfExample.Models
+{
+    public class TaskModel
+    {
+        public int Id { get; set; }
+        public DateTime CreateDate { get; set; }
+        public DateTime DueDate { get; set; }
+        public DateTime? CompletionDate { get; set; }
+        public string TaskTitle { get; set; }
+        public string TaskDiscription { get; set; }
+    }
+}
